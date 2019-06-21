2bfa81579a90756005237c536e4d496a8527e430
Zeljko Bozic <zeljko.bozic@quantox.com>
2019-06-21 09:32:22 +0200

    Adding app router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "./App.scss";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Quiz() {
+  return <h2>Quiz</h2>;
+}
+
+function Results() {
+  return <h2>Results</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/quiz/">Quiz</Link>
+            </li>
+            <li>
+              <Link to="/results/">Results</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/quiz/" component={Quiz} />
+        <Route path="/results/" component={Results} />
+      </div>
+    </Router>
   );
 }
 
