001f92b99e6a270962b7de42134b7bf5ab43a7b3
Zeljko Bozic <zeljko.bozic@quantox.com>
2019-06-24 12:14:44 +0200

    Wired local storage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/home.scss";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default function Home() {
-  const scores = [
-    { date: "on 06/13/2019", score: "2,300 pts" },
-    { date: "on 06/16/2019", score: "1,900 pts" },
-    { date: "on 06/09/2019", score: "900 pts" }
-  ];
-  return (
-    <div>
-      <h3>CONTINENT QUIZ</h3>
-      <h1>Your Scores</h1>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.scores = JSON.parse(localStorage.getItem("topScores"));
+    this.scores = this.scores.slice(0, 3);
+  }
+
+  componentDidMount() {
+    this.props.dispatch({
+      type: "ADD_QUESTIONS_STARTED"
+    });
+  }
+
+  render() {
+    return (
       <div>
-        {scores.map((item, index) => (
-          <div className="score-item">
-            <div className="score-index ">#{index + 1}</div>
-            <div>
-              <h3>{item.date}</h3>
-              <h2>{item.score}</h2>
-            </div>
+        <h3>CONTINENT QUIZ</h3>
+        <h1>Your Scores</h1>
+        <div>
+          {this.scores
+            ? this.scores.map((item, index) => (
+                <div key={index} className="score-item">
+                  <div className="score-index ">#{index + 1}</div>
+                  <div>
+                    <h3>on {item.date}</h3>
+                    <h2>{item.score} pts</h2>
+                  </div>
+                </div>
+              ))
+            : "No scores yet"}
+        </div>
+        <div className="bottom-section">
+          <div className="home-button">icon</div>
+          <div className="play-button">
+            <div>icon</div>
+            <h2>
+              <Link to="/quiz/">Play</Link>
+            </h2>
           </div>
-        ))}
-      </div>
-      <div className="bottom-section">
-        <div className="home-button">icon</div>
-        <div className="play-button">
-          <div>icon</div>
-          <h2>
-            <Link to="/quiz/">Play</Link>
-          </h2>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default connect()(Home);
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import "../styles/result.scss";
 import { Link } from "react-router-dom";
 
+const handleLocalStorage = (score, date) => {
+  let scores = JSON.parse(localStorage.getItem("topScores"));
+  if (scores) {
+    let newScores = [...scores, { score, date }];
+    newScores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+    localStorage.setItem("topScores", JSON.stringify(newScores));
+  } else {
+    localStorage.setItem("topScores", JSON.stringify([{ score, date }]));
+  }
+};
+
 export default function Results(props) {
   return (
     <div>
@@ -12,7 +25,15 @@ export default function Results(props) {
         <h1>Your Score</h1>
         <div className="total-score">{`${props.location.state.score} pts`}</div>
         <div className="finish-button">
-          <Link to="/">Finish</Link>
+          <Link
+            to="/"
+            onClick={() => {
+              const date = new Date();
+              handleLocalStorage(props.location.state.score, date.getTime());
+            }}
+          >
+            Finish
+          </Link>
         </div>
       </div>
     </div>
